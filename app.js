b6173dea31a8dd81452a171ded0814e12b148adc
yougottafree <44795110+yougottafree@users.noreply.github.com>
2021-04-05 16:47:34 -0700

    make some changes

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+buttons = document.getElementsByClassName("click-section");
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function(){
+        alert("Under construction");
+    })
+}
